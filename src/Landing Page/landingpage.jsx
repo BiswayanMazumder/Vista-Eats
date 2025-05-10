cc886ffa18ba47c1dd184b960b3549bad102bfa4
BiswayanMazumder <biswayanmazumder27@gmail.com>
2025-05-10 16:45:32 +0530

    Login Page Creation

--- src/Landing Page/landingpage.jsx
+++ src/Landing Page/landingpage.jsx
@@ -1,23 +1,43 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 
 export default function Landingpage() {
     useEffect(() => {
         document.title = "Order Food & Groceries. Discover the best restaurants.";
     }, []);
+    const [showLogin, setShowLogin] = useState(false);
+    const toggleLogin = () => {
+        setShowLogin(prev => !prev);
+    };
     return (
         <div className='webbody'>
             <div className="banner1">
-            <div className="headers">
-                <Link to="/">
-                <img src="https://firebasestorage.googleapis.com/v0/b/wingedwordsadmin.appspot.com/o/Vista%20Eats%2FChatGPT%20Image%20May%2010%2C%202025%2C%2001_32_03%20PM.png?alt=media&token=d04e9a1b-11a7-4816-848f-f91099f3b1af" alt="" height={"70px"} width={"90px"} />
-                </Link>
-                <Link style={{ textDecoration: "none" }} to="/login">
-                    <div className="loginbtn">
-                        Sign in
+                {showLogin && <div className="blur-overlay" onClick={toggleLogin}></div>}
+
+                {/* Sidebar login section */}
+                {showLogin && (
+                    <div className="loginsection">
+                        <div className="jhdcjdhc">
+                            <div className="dgvdbdc">
+                                Login
+                                <div className="dfhdf">
+                                    or create an account
+                                </div>
+                            </div>
+                            <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/Image-login_btpq7r" alt="" height={"80px"} width={"80px"} style={{marginRight:"100px"}}/>
+                        </div>
                     </div>
-                </Link>
-            </div>
+                )}
+                <div className="headers">
+                    <Link to="/">
+                        <img src="https://firebasestorage.googleapis.com/v0/b/wingedwordsadmin.appspot.com/o/Vista%20Eats%2FChatGPT%20Image%20May%2010%2C%202025%2C%2001_32_03%20PM.png?alt=media&token=d04e9a1b-11a7-4816-848f-f91099f3b1af" alt="" height={"70px"} width={"90px"} />
+                    </Link>
+                    <Link style={{ textDecoration: "none" }}>
+                        <div className="loginbtn" onClick={toggleLogin}>
+                            Sign in
+                        </div>
+                    </Link>
+                </div>
                 <div className="banner1_images">
                     <img
                         src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/testing/seo-home/Veggies_new.png"
@@ -27,23 +47,23 @@ export default function Landingpage() {
 
                     <div className="banner_txt">
 
-                        Order food & groceries. Discover <br/>best restaurants. Vista it!
+                        Order food & groceries. Discover <br />best restaurants. Vista it!
                         <Link to="/search" style={{ textDecoration: "none" }}>
-                        <div className="restaurants_input">
-                            <div className='dhfhdfjdfn'>
-                                Search for restaurants, dishes, cuisines
+                            <div className="restaurants_input">
+                                <div className='dhfhdfjdfn'>
+                                    Search for restaurants, dishes, cuisines
+                                </div>
                             </div>
-                        </div>
                         </Link>
                         <div className="dhfhjdhfjefh">
                             <Link>
-                            <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/23/ec86a309-9b06-48e2-9adc-35753f06bc0a_Food3BU.png" alt="" height={"350px"} width={"400px"} />
+                                <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/23/ec86a309-9b06-48e2-9adc-35753f06bc0a_Food3BU.png" alt="" height={"350px"} width={"400px"} />
                             </Link>
                             <Link>
-                            <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/23/b5c57bbf-df54-4dad-95d1-62e3a7a8424d_IM3BU.png" alt="" height={"350px"} width={"400px"} />
+                                <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/23/b5c57bbf-df54-4dad-95d1-62e3a7a8424d_IM3BU.png" alt="" height={"350px"} width={"400px"} />
                             </Link>
                             <Link>
-                            <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/23/b6d9b7ab-91c7-4f72-9bf2-fcd4ceec3537_DO3BU.png" alt="" height={"350px"} width={"400px"} />
+                                <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/23/b6d9b7ab-91c7-4f72-9bf2-fcd4ceec3537_DO3BU.png" alt="" height={"350px"} width={"400px"} />
                             </Link>
                         </div>
                     </div>
